262387a04abc03156900e9920722a27066830257
varsha21297 <varsha21297@iiitd.ac.in>
2023-09-10 11:32:03 +0530

    hello

--- Assignment2/fib.c
+++ Assignment2/fib.c
@@ -8,8 +8,10 @@ int fib(int n) {
 }
 
 int main(int argc, char *argv[]){
-    int n= argv[1];
-    int result = fib(n);
-    printf("fib(%d) = %d\n", n, result);
+    int n = 0;
+    if (argc == 2) {
+        n = atoi(argv[1]);
+    }
+    printf("fib(%d) = %d\n", n, fib(n));
     return 0;
 }
