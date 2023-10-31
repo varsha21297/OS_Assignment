3c6738da2bf50e46cb897997a04b3f6373f8199e
varsha21297 <varsha21297@iiitd.ac.in>
2023-10-31 21:41:52 +0530

    bahk

--- SmartLoader.c
+++ SmartLoader.c
@@ -4,45 +4,33 @@ Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
 int fd;
 
-/*
- * Release memory and other cleanups
- */
 void loader_cleanup() {
-    free(ehdr);
-    free(phdr);
-    close(fd);
+        free(ehdr);
+        free(phdr);
+        close(fd);
+    
 }
 
-/*
- * Load and run the ELF executable file
- */
 void load_and_run_elf(char **exe) {
-    // Open the ELF file
-    fd = open(exe[1], O_RDONLY);
+    fd = open(exe, O_RDONLY);
     if (fd == -1) {
-        perror("Error opening ELF file");
-        exit(1);
+        perror("Error opening file");
+        return;
     }
 
-    // Get the file size
+    //Get the file size
     off_t fileSize = lseek(fd, 0, SEEK_END);
     lseek(fd, 0, SEEK_SET);  // Reset file offset to the beginning
 
-    // Allocate memory to store the entire ELF file
-    ehdr = (Elf32_Ehdr *)malloc(fileSize);
+    // Read ELF header
+    ehdr = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
     if (ehdr == NULL) {
         perror("Error allocating memory for ELF header");
         close(fd);
-        exit(1);
+        return;
     }
-
-    if (read(fd, phdr, ehdr->e_phnum * sizeof(Elf32_Phdr)) != ehdr->e_phnum * sizeof(Elf32_Phdr)) {
-      fprintf(stderr, "Error reading program headers\n");
-      free(ehdr);
-      free(phdr);
-      close(fd);
-      exit(1);
-    }
+    
+    read(fd, ehdr, sizeof(Elf32_Ehdr));
 
     // Calculate the offset of the program headers
     unsigned int p_off = ehdr->e_phoff;
@@ -50,84 +38,65 @@ void load_and_run_elf(char **exe) {
     // Calculate the number of program headers
     unsigned short p_num = ehdr->e_phnum;
 
-    // Allocate memory to store program headers
-    phdr = (Elf32_Phdr *)malloc(p_num * sizeof(Elf32_Phdr));
-    if (phdr == NULL) {
-        perror("Error allocating memory for program headers");
-        free(ehdr);
-        close(fd);
-        exit(1);
-    }
+    // Calculate the size of each program header
+    unsigned short p_size = ehdr->e_phentsize;
 
-    // Read program headers
-    if (lseek(fd, p_off, SEEK_SET) == -1) {
-        perror("Error seeking to program headers");
-        free(ehdr);
-        free(phdr);
-        close(fd);
-        exit(1);
-    }
-    if (read(fd, phdr, p_num * sizeof(Elf32_Phdr)) != p_num * sizeof(Elf32_Phdr)) {
-        perror("Error reading program headers");
-        free(ehdr);
-        free(phdr);
-        close(fd);
-        exit(1);
-    }
-
-    // Find the entry point segment
     unsigned int entry_point = ehdr->e_entry;
-    unsigned int address = 0;
-    int offset = 0;
+  
 
-    for (int i = 0; i < p_num; i++) {
-        if (phdr[i].p_type == PT_LOAD) {
-            if (entry_point >= phdr[i].p_vaddr && entry_point < phdr[i].p_vaddr + phdr[i].p_memsz) {
-                address = phdr[i].p_vaddr;
-                offset = phdr[i].p_offset;
-                break;
-            }
-        }
+    // Find the PT_LOAD segment with entrypoint
+    phdr = (Elf32_Phdr *)malloc(p_size);   //CHANGED
+    if (phdr == NULL) {
+        perror("Error allocating memory for program header");
+        loader_cleanup();
+        return;
     }
 
-    // Allocate memory for the entry point segment
-    void *virtual_mem = mmap(NULL, phdr->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    lseek(fd, p_off, SEEK_SET);
+
+    unsigned int address;
+    int offset;
+    
+    for (int i = 0; i < p_num; i++) {
+    read(fd, phdr, p_size);
+    unsigned int type = phdr -> p_type;
+        if (type==PT_LOAD) { 
+        if ((entry_point >= phdr->p_vaddr) && (entry_point <= phdr->p_vaddr + phdr->p_memsz)) {
+          break;
+            //found segment 
+        }
+      }
+    }
+
+    // Allocate memory using mmap
+    void *virtual_mem = mmap(NULL, phdr->p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC,
+                                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
     if (virtual_mem == MAP_FAILED) {
-        perror("Error allocating memory for the entry point segment");
-        free(ehdr);
-        free(phdr);
-        close(fd);
-        exit(1);
-    }
+    perror("Error mapping memory for segment");
+    free(ehdr);
+    free(phdr);
+    close(fd);
+    exit(1);
+}
 
-    void *actual = (void *)((char *)virtual_mem + entry_point - (address + offset));
+    // Copy segment content
+    lseek(fd, phdr->p_offset, SEEK_SET);
+    read(fd, virtual_mem, phdr->p_filesz);
 
-    // Copy the segment of PT_LOAD
-    if (lseek(fd, offset, SEEK_SET) == -1) {
-        perror("Error seeking to the entry point segment");
-        free(ehdr);
-        free(phdr);
-        close(fd);
-        munmap(virtual_mem, phdr->p_memsz);
-        exit(1);
-    }
+    // Calculate entrypoint address within the loaded segment
+    void *actual = virtual_mem + (ehdr->e_entry - phdr->p_vaddr);
 
-    if (read(fd, virtual_mem, phdr->p_filesz) != phdr->p_filesz) {
-        perror("Error reading the entry point segment");
-        free(ehdr);
-        free(phdr);
-        close(fd);
-        munmap(virtual_mem, phdr->p_memsz);
-        exit(1);
-    }
-
-    // Define and call the _start function
+    // Define function pointer type for _start and use it to typecast function pointer properly
     typedef int (*StartFunc)();
     StartFunc _start = (StartFunc)actual;
-    int result = _start();
 
+    // Call the "_start" method and print the value returned from "_start"
+    int result = _start();
     printf("User _start return value = %d\n", result);
+
+    // Cleanup whatever opened
+    munmap(virtual_mem, phdr->p_memsz);
 }
 
 int main(int argc, char **argv) {
@@ -135,10 +104,7 @@ int main(int argc, char **argv) {
         printf("Usage: %s <ELF Executable>\n", argv[0]);
         exit(1);
     }
-    // 1. Carry out necessary checks on the input ELF file
-    // 2. Pass it to the loader for carrying out the loading/execution
-    load_and_run_elf(argv);
-    // 3. Invoke the cleanup routine inside the loader
-    loader_cleanup(ehdr, phdr);
+    load_and_run_elf(argv[1]);
+    loader_cleanup();
     return 0;
 }
