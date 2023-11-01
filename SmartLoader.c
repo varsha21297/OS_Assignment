7d8a8291d01b528e9e181d5361b3c7de8e43c8f3
varsha21297 <varsha21297@iiitd.ac.in>
2023-11-01 20:04:15 +0530

    febwk

--- SmartLoader.c
+++ SmartLoader.c
@@ -5,37 +5,60 @@
 
 Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
-Elf32_Phdr *segment
 int fd;
-void *virtual_mem;
+void* virtual_mem;
 
 void loader_cleanup() {
-    free(ehdr);
-    free(phdr);
-    close(fd);
-    if (virtual_mem != MAP_FAILED) {
-        munmap(virtual_mem, phdr->p_memsz);
-    }
+        free(ehdr);
+        free(phdr);
+        close(fd);
+    
 }
 
-void my_handler(int sig, siginfo_t *info, void *context) {
+void my_handler(int sig, siginfo_t *info, void *context){ //function to handle SIGSEV signal
     if (sig == SIGSEGV) {
         printf("Segmentation fault caught!\n");
-        void *faulty = info->si_addr;
+        siginfo_t *info;
+        void *faulty= info->si_addr;
+        ehdr = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
+        if (ehdr == NULL) {
+            perror("Error allocating memory for ELF header");
+            close(fd);
+            return;
+        }
+        
+        read(fd, ehdr, sizeof(Elf32_Ehdr));
 
-        for (int i = 0; i < ehdr->e_phnum; i++) {
-            segment = (Elf32_Phdr *)((char *)ehdr + ehdr->e_phoff + i * ehdr->e_phentsize);
-            if (faulty >= (void *)segment->p_vaddr && faulty < (void *)(segment->p_vaddr + segment->p_memsz)) {
-                size_t calc_mem = ((segment->p_memsz + 4096 - 1) / 4096) * 4096;
-                virtual_mem = mmap(NULL, calc_mem, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        // Calculate the offset of the program headers
+        unsigned int p_off = ehdr->e_phoff;
+
+        // Calculate the number of program headers
+        unsigned short p_num = ehdr->e_phnum;
+
+        // Calculate the size of each program header
+        unsigned short p_size = ehdr->e_phentsize;
+
+        unsigned int entry_point = ehdr->e_entry;
+        lseek(fd, p_off, SEEK_SET);
+        for (int i = 0; i < p_num; i++) {
+            read(fd, phdr, p_size);
+            if ((faulty >= phdr->p_vaddr) && (faulty <= phdr->p_vaddr + phdr->p_memsz)) {
+                size_t calc_mem= ((phdr->p_memsz +4096-1)/4096)*4096;
+                void *virtual_mem = mmap(NULL, calc_mem, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+                int result = _start();
+                printf("User _start return value = %d\n", result);
+                // Cleanup whatever opened
+                munmap(virtual_mem, phdr->p_memsz);
                 if (virtual_mem == MAP_FAILED) {
                     perror("Error allocating memory for segment");
                     loader_cleanup();
-                    exit(1);
+                    return;
                 }
-                return;
+                break;
+                //found segment 
             }
-        }
+        
+    }
     }
 }
 
@@ -46,24 +69,34 @@ void load_and_run_elf(char **exe) {
         return;
     }
 
+    //Get the file size
+    off_t fileSize = lseek(fd, 0, SEEK_END);
+    lseek(fd, 0, SEEK_SET);  // Reset file offset to the beginning
+
+    // Read ELF header
     ehdr = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
     if (ehdr == NULL) {
         perror("Error allocating memory for ELF header");
         close(fd);
         return;
     }
+    
+    read(fd, ehdr, sizeof(Elf32_Ehdr));
 
-    if (read(fd, ehdr, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr)) {
-        perror("Error reading ELF header");
-        loader_cleanup();
-        return;
-    }
-
+    // Calculate the offset of the program headers
     unsigned int p_off = ehdr->e_phoff;
+
+    // Calculate the number of program headers
     unsigned short p_num = ehdr->e_phnum;
+
+    // Calculate the size of each program header
     unsigned short p_size = ehdr->e_phentsize;
 
-    phdr = (Elf32_Phdr *)malloc(p_size);
+    unsigned int entry_point = ehdr->e_entry;
+  
+
+    // Find the PT_LOAD segment with entrypoint
+    phdr = (Elf32_Phdr *)malloc(p_size);   //CHANGED
     if (phdr == NULL) {
         perror("Error allocating memory for program header");
         loader_cleanup();
@@ -72,21 +105,22 @@ void load_and_run_elf(char **exe) {
 
     lseek(fd, p_off, SEEK_SET);
 
+    unsigned int address;
+    int offset;
+    // Calculate entrypoint address within the loaded segment
     void *actual = ehdr->e_entry;
 
+    // Define function pointer type for _start and use it to typecast function pointer properly
     typedef int (*StartFunc)();
     StartFunc _start = (StartFunc)actual;
 
-    virtual_mem = NULL;
-
-    if (signal(SIGSEGV, my_handler) == SIG_ERR) {
+    // Call the "_start" method and print the value returned from "_start"
+    int result = _start();
+    if (signal(SIGSEGV, my_handler) == SIG_ERR) { //handle SIGSEGV signal
         perror("Error handling SIGSEGV");
         loader_cleanup();
         return;
     }
-
-    int result = _start();
-
     printf("User _start return value = %d\n", result);
 }
 
