fde079f24f36878c9e8690c323baac54499c7ebd
varsha21297 <varsha21297@iiitd.ac.in>
2023-11-03 20:22:44 +0530

    done

--- Assignment4/fib.c
+++ Assignment4/fib.c
@@ -1,18 +1,12 @@
-#include <stdio.h>
-
+/*
+ * No changes are allowed in this file
+ */
 int fib(int n) {
-    if (n == 0 || n == 1) {
-        return n;
-    }
-    return fib(n - 1) + fib(n - 2);
+  if(n<2) return n;
+  else return fib(n-1)+fib(n-2);
 }
 
-
-int main(int argc, char *argv[]){
-    int n = 0;
-    if (argc == 2) {
-        n = atoi(argv[1]);
-    }
-    printf("fib(%d) = %d\n", n, fib(n));
-    return 0;
+int _start() {
+	int val = fib(40);
+	return val;
 }
